b118726b8a95b19e65fb30e1061469e7371c2f23
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2025-12-11 18:48:19 +0700

    Update lab6_1.cpp
    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,23 @@
 using namespace std;
 
 int main(){
+    int even = 0;
+    int odd = 0;
+    int a = 0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> a;
+        while(a != 0){
+            if (a % 2 == 0){
+                even ++;
+            }
+            if (a % 2 == 1){
+                odd ++;
+            }
+            cout << "Enter an integer: ";
+            cin >> a;
+            
+        }
+    cout << "#Even numbers = " << even <<"\n";
+    cout << "#Odd numbers = " << odd <<"\n";
     return 0;
 }
